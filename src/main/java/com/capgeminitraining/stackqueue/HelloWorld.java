cbd541dcf5364715703da82a7b589401f9b2572c
Patel210 <pranavpatel2101997@gmail.com>
2020-10-08 08:24:29 +0530

    Added: Hello World program to check project configuration and pom file with dependencies for the project

---
+++ src/main/java/com/capgeminitraining/stackqueue/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.capgeminitraining.stackqueue;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
